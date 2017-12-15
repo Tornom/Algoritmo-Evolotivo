2c380c291d434c0cc037f8e221a0c459a13b12e7
Tornom <lucasalbao@gmail.com>
2017-12-15 18:26:32 -0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,22 +1,23 @@
-#include <allegro5/allegro.h>                       //inclue as principais funÁıes da allegro 5;
-#include <allegro5/allegro_image.h>                 //inclue manipulaÁ„o e manuseio de imagens;
-#include <allegro5/allegro_primitives.h>            //inclue manipulaÁ„o e manuseio de primitivas;
-#include <allegro5/allegro_font.h>                  //inclue manipulaÁ„o e manuseio de fontes;
-#include <allegro5/allegro_ttf.h>                   //inclue manipulaÁ„o e manuseio do formato .ttf;
+#include <allegro5/allegro.h>                       //inclue as principais fun√ß√µes da allegro 5;
+#include <allegro5/allegro_image.h>                 //inclue manipula√ß√£o e manuseio de imagens;
+#include <allegro5/allegro_primitives.h>            //inclue manipula√ß√£o e manuseio de primitivas;
+#include <allegro5/allegro_font.h>                  //inclue manipula√ß√£o e manuseio de fontes;
+#include <allegro5/allegro_ttf.h>                   //inclue manipula√ß√£o e manuseio do formato .ttf;
 #include <allegro5/allegro_audio.h>
 #include <allegro5/allegro_acodec.h>
 #include <allegro5/display.h>
 #include <math.h>
+#include <stdio.h>
 
-//valores para a parte gr·fica
-#define FPS 450.0                                   //necess·rio a presenÁa do ".0" ||450||;
-#define largura 1000
-#define altura 800
+//valores para a parte gr√°fica
+#define FPS 450.0                                   //necess√°rio a presen√ßa do ".0" ||450||;
+#define largura 1300
+#define altura 820
 
-//valores para a simulaÁ„o
-#define max_pop 10
-#define num_max_movimento 200
-#define taxa_muta 5
+//valores para a simula√ß√£o
+#define max_pop 40
+#define num_max_movimento 300
+#define taxa_muta 50
 #define quanto_muta 1
 #define nota_pela_vitoria 20
 #define nota_pela_derrota 10
@@ -24,27 +25,32 @@
 #define numero_bala 3
 #define tempo_para_apagar_bala FPS*5
 #define geracao_preda 10
+#define valor_inicial 999999
+#define coef 200
 
 //valores maximos player
-#define maximo_prot 3
-#define max_atk_spd 5
+#define maximo_prot 6
+#define max_atk_spd 10
 #define max_dano 10
-#define max_range_corpo 110
-#define max_range_range 200
+#define max_range_corpo 50
+#define max_range_range 400
+#define crit_max 0
 
 //minimo player
 #define min_prot 0
-#define min_atk_spd 1
+#define min_atk_spd 2
 #define min_atk_spd_rang 2
-#define min_dano 4
-#define min_range_corpo 30
-#define min_range_range 90
+#define min_dano 1
+#define min_range_corpo 20
+#define min_range_range 150
 
 //valores fixos para o player
-#define vel_corpo 11
+#define vel_corpo 8
+#define vel_range 8
 #define maior_hp 10
 #define turno_b  20
-#define tamanho_blinky 40
+#define tamanho_blinky 0
+#define cte_critico 3000
 
 
 
@@ -92,13 +98,13 @@ typedef struct                  //struct para a sprite;
     short movy;                 //sentido Y da sprite;
 
     int framemax;               //numero maximo de frames da sprite;
-    int frameatual;             //valor do frame atual da animaÁ„o;
-    int framecount;             //contador para atrazo de animaÁ„o;
-    int frameatrazo;            //valor de atrazo de animaÁ„o;
+    int frameatual;             //valor do frame atual da anima√ß√£o;
+    int framecount;             //contador para atrazo de anima√ß√£o;
+    int frameatrazo;            //valor de atrazo de anima√ß√£o;
     int framelargura;           //lagura da sprite;
     int framealtura;            //altura da sprite;
-    int colunaanima;            //quantas colunas possui a animaÁ„o;
-    int movanima;               //define em qual sentido a sprite ira mover sua animaÁao;
+    int colunaanima;            //quantas colunas possui a anima√ß√£o;
+    int movanima;               //define em qual sentido a sprite ira mover sua anima√ßao;
 
     ALLEGRO_BITMAP *imagem;     //sprite;
     ALLEGRO_BITMAP *anima[10];
@@ -111,7 +117,8 @@ typedef struct
     int hp;
     int maxhp;
     int protecao;
-    int nota;
+    int crit;
+    float nota;
     int blink;
     int tamanho_blink;
     fanta imagem;
@@ -137,5 +144,8 @@ void move_projetil(popula &one);
 
 bool avalia_populacao(popula &one,popula &two,bool empate, int conta);
 
+void anima_player(popula &one);
+
+void calibra_fanta(popula &one);
 
 
